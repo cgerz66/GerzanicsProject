a8ebf0d5f94e1f3d5acd5e37c81f1f9ffab08320
charlotte <cgerz66@gmail.com>
2019-05-30 10:20:02 -0700

    new commit

--- HABRproject.R
+++ HABRproject.R
@@ -3,4 +3,20 @@
 library(dplyr)
 library(ggplot2)
 
-HAB_data<- read.csv("HABRproject/data/habsos_20190211.csv")
+HAB_data<- read.csv("../data/habsos_20190211.csv")
+print(HAB_data)
+
+ggplot(HAB_data, aes(x=sample_depth, y=cellcount)) + 
+  geom_point(size=3, color = "blue", alpha = 0.5)
+
+ggplot(HAB_data, aes(x=salinity, y=cellcount)) + #create plot of sample date and salinity
+  geom_point(size=3, color = "blue", alpha = 0.5) +
+  labs(x = "salinity (ppt)", y = "cell count")
+
+head(HAB_data)
+  
+HAB_data%>% mutate(water_temp_qa = NULL)
+HAB_data%>% mutate(wind_dir_qa = NULL)
+
+
+
